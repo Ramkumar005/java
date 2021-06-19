8a77c3306f32b4560baf89b3029b93bf9533c795
Ramkumar005 <63731813+Ramkumar005@users.noreply.github.com>
2021-06-19 21:15:32 +0530

    Add files via upload

---
+++ OddOrEven.java
@@ -0,0 +1,14 @@
+package week1_Day1.Assignment;
+
+public class OddOrEven {
+
+	public static void main(String[] args) {
+
+		int num = 4;
+		if (num % 2 == 0) {
+			System.out.println("even number");
+		} else {
+			System.out.println("odd number");
+		}
+	}
+}
